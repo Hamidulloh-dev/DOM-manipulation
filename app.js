6aeaf30f8d559672e75dbc20696e62993a3d0a39
hamidulloh-dev <hamidullohmuhammadjonov123@gmail.com>
2025-07-10 10:09:04 +0500

    commit

--- app.js
+++ app.js
@@ -1,7 +1,44 @@
-const red = document.querySelector('.red')
-const yellow = document.querySelector('.yellow')
-const green = document.querySelector('.green')
-
 class Svetafor {
-    
+  constructor() {
+    this.red = document.querySelector(".red");
+    this.yellow = document.querySelector(".yellow");
+    this.green = document.querySelector(".green");
+  }
+
+  allOff() {
+    this.red.classList.remove("active");
+    this.yellow.classList.remove("active");
+    this.green.classList.remove("active");
+  }
+
+  start() {
+    this.redLight();
+  }
+
+  redLight() {
+    this.allOff();
+    this.red.classList.add("active");
+    setTimeout(() => this.yellowAfterGreen(), 3000);
+  }
+
+  yellowAfterGreen() {
+    this.allOff();
+    this.yellow.classList.add("active");
+    setTimeout(() => this.greenLight(), 2000);
+  }
+
+  greenLight() {
+    this.allOff();
+    this.green.classList.add("active");
+    setTimeout(() => this.yellowAfterRed(), 3000);
+  }
+
+  yellowAfterRed() {
+    this.allOff();
+    this.yellow.classList.add("active");
+    setTimeout(() => this.redLight(), 2000);
+  }
 }
+
+const svetafor = new Svetafor();
+svetafor.start();
